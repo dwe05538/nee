efff34a4a9e877306c4ad98a0d5c2e5f1963d788
SDWE12345 <xx5190443@gmail.com>
2025-07-02 04:00:52 +0530

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,17 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-    <script src="https://cdn.tailwindcss.com"></script>
+      <Head />   <Script
+        src="https://cdn.tailwindcss.com"
+        strategy="afterInteractive" // or 'lazyOnload'
+        onLoad={() => {
+          console.log('Script has loaded');
+          // Initialize script here
+        }}
+      />
       <body>
         <Main />
         <NextScript />
